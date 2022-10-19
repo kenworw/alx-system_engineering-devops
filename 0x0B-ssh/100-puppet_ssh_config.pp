ce30634fa069fb5f73eb9073b103e59de2213329
kenworw <kenworw@users.noreply.github.com>
2022-10-19 19:56:21 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,19 @@
-# use puppet to make changes to configuration file
-
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
+# create ssh config file
+file { '/etc/ssh/ssh_config':
+	ensure => file
 }
+
+file_line { 'identitiy_file':
+	ensure => created,
+	path => '/etc/ssh/ssh_config',
+	match => 'IdentityFile',
+	line => 'IdentityFile ~/.ssh/school'
+}
+
+file_line { 'refuse_password':
+	ensure => created,
+	path => '/etc/ssh/ssh_config',
+	match => 'PasswordAuthentication',
+	line => 'PasswordAuthentication no'
+}
+	
